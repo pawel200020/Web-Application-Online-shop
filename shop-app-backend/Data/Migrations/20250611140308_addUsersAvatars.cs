a45bf5e2b5d72d7f5b9352680c4649aa78e354e5
pawel200020 <pawel200020@gmail.com>
2025-06-11 16:06:22 +0200

    🗃️Avatars table

---
+++ shop-app-backend/Data/Migrations/20250611140308_addUsersAvatars.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUsersAvatars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAvatars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<byte>(type: "tinyint", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TsInsert = table.Column<DateTime>(type: "datetime2", nullable: false, computedColumnSql: "getutcdate()"),
+                    TsUpdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAvatars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAvatars_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAvatars_UserId",
+                table: "UserAvatars",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAvatars");
+        }
+    }
+}
